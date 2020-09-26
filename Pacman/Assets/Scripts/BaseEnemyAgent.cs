a4616aa4f313f85c86ff088621148945b0de0aa0
2380carlos <carlos.2380@hotmail.com>
2020-09-26 21:24:32 +0200

    Basic enemy navigation

---
+++ Pacman/Assets/Scripts/BaseEnemyAgent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BaseEnemyAgent : MonoBehaviour
+{
+    public Transform waypoint1;
+    public Transform waypoint2;
+
+
+    private NavMeshAgent agent;
+    private int x;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.SetDestination(waypoint2.position);
+        x = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(agent.velocity.x) > Mathf.Abs(agent.velocity.z))
+        {
+            transform.rotation = Quaternion.LookRotation(new Vector3(agent.velocity.x, 0, 0));
+        }else
+        {
+            transform.rotation = Quaternion.LookRotation(new Vector3(0, 0, agent.velocity.z));
+        }
+
+        if(agent.hasPath == false && agent.pathPending == false)
+        {
+            if(x == 2)
+            {
+                agent.SetDestination(waypoint1.position);
+                x = 1;
+            }
+            else
+            {
+                x = 2;
+                agent.SetDestination(waypoint2.position);
+            }
+        }
+    }
+}
