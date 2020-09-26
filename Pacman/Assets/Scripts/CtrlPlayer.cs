a96ac252056280a073aded441395206a1cbc3dce
2380carlos <carlos.2380@hotmail.com>
2020-09-27 00:27:01 +0200

    Forward movment player

---
+++ Pacman/Assets/Scripts/CtrlPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlPlayer : MonoBehaviour
+{
+    public float speed;
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = new Vector3(1, 0, 0);
+        gameObject.transform.LookAt(velocity.normalized);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        float maxDistHit = 0.5f;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, maxDistHit))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * maxDistHit, Color.white);
+            Debug.Log("Did not Hit");
+            gameObject.transform.LookAt(velocity.normalized + transform.position);
+            gameObject.transform.position += velocity * speed * Time.deltaTime;
+        }
+        
+    }
+
+
+}
